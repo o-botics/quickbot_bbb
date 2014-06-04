3f0d7bb5046975cefe89a48329465a4419309e4e
Rowland O'Flaherty <rowoflo@gmail.com>
2014-06-04 08:24:32 -0400

    Updated serial script to be able to run on startup. Instructions on setting up serial script to run on startup are at http://o-botics.org/robots/quickbot/mooc/v2/configuring.html

--- serial/setup.sh
+++ serial/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-cp *.dtbo /lib/firmware
+cp ~/quickbot_bbb/serial/ttyO1_armhf.com-00A0.dtbo /lib/firmware
+cp ~/quickbot_bbb/serial/ttyO2_armhf.com-00A0.dtbo /lib/firmware
+cp ~/quickbot_bbb/serial/ttyO4_armhf.com-00A0.dtbo /lib/firmware
+cp ~/quickbot_bbb/serial/ttyO5_armhf.com-00A0.dtbo /lib/firmware
+
 echo ttyO1_armhf.com > /sys/devices/bone_capemgr*/slots
 echo ttyO2_armhf.com > /sys/devices/bone_capemgr*/slots
 echo ttyO4_armhf.com > /sys/devices/bone_capemgr*/slots
 echo ttyO5_armhf.com > /sys/devices/bone_capemgr*/slots
-./serialSetup.py
+~/quickbot_bbb/serial/serialSetup.py
 
